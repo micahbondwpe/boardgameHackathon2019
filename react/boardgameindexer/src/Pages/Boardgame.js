d67aadba3e42edfcc6b6ae70582db5d6c906d170
Gary Singer <gary.singer@wpengine.com>
2019-11-05 11:18:56 -0600

    finished core boardgame page - Gary

--- react/boardgameindexer/src/Pages/Boardgame.js
+++ react/boardgameindexer/src/Pages/Boardgame.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
-//import axios from "axios"
+import axios from "axios"
 import queryString from 'query-string'
+import "../css/style.css"
 
 class Boardgame extends Component {
     constructor() {
@@ -12,11 +13,31 @@ class Boardgame extends Component {
     componentDidMount() {
         const values = queryString.parse(this.props.location.search)
         console.log(values.id)
+        axios.get(`http://localhost:5000/boardgame/${values.id}`).then(response => {
+            this.setState({ 
+                "age": response.data.age,
+                "boardgamecategory": response.data.boardgamecategory,
+                "boardgamemechanic": response.data.boardgamemechanic, 
+                "id": response.data.id, 
+                "image": response.data.image, 
+                "maxplayers": response.data.maxplayers, 
+                "minplayers": response.data.minplayers, 
+                "name": response.data.name, 
+                "playingtime": response.data.playingtime, 
+                "rating": response.data.rating, 
+                "thumbnail": response.data.thumbnail
+            });
+        });
       }
     render() {
         return(
             <div>
-                
+                <h2>{this.state.name}</h2>
+                <img src={this.state.thumbnail} alt={this.state.name}></img>
+                <p>Rating: {this.state.rating}</p>
+                <p>Players: {this.state.minplayers} - {this.state.maxplayers}</p>
+                <p>Average Playtime: {this.state.playingtime}</p>
+                <p>Minimum Age: {this.state.age}</p>
             </div>
         )
     }
